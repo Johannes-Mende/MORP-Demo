681deb5cb7424edd2180a1ba5d50d0b4483b07ac
Johannes Mende <jomende99@gmx.de>
2021-10-08 11:04:22 +0200

    Healthbar

--- Assets/C#/UI/Statusbar.cs
+++ Assets/C#/UI/Statusbar.cs
@@ -5,15 +5,27 @@ using UnityEngine.UI;
 
 public class Statusbar : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Slider slider;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
     }
 }
